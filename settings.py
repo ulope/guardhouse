f1f2d210eeef4bd8e383ed08e0a6bdb03ef22d8c
Ulrich Petri <ulo@ulo.pe>
2011-07-30 04:57:45 +0200

    more settings

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 # Django settings for guardhouse project.
 
+from os import path
+SITE_ROOT = path.dirname(path.realpath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Ulrich Petri', 'guardhouse@ulo.pe'),
 )
 
 MANAGERS = ADMINS
@@ -20,81 +23,50 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Berlin'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path.join(SITE_ROOT, "media")
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path.join(SITE_ROOT, "web")
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    path.join(SITE_ROOT, "static"),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 try:
-    with open(".sdecret", "r") as secret:
+    # load secret key from file to keep it out of vcs
+    with open(".secret", "r") as secret:
         SECRET_KEY = secret.read()
 except IOError:
     from django.core.exceptions import ImproperlyConfigured
-    raise ImproperlyConfigured("You need to place a secret key in a file called '.secret' in the project root.")
+    raise ImproperlyConfigured(
+        "You need to place a secret key in a file called '.secret' in the "
+        "project root.\nFor example:\n\t$ ./manage.py generate_secret_key > .secret"
+    )
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -108,9 +80,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'guardhouse.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    path.join(SITE_ROOT, "templates"),
 )
 
 INSTALLED_APPS = (
@@ -123,7 +93,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django_extensions',
     'compressor',
-    
+
+    'content',
 )
 
 # A sample logging configuration. The only tangible logging
