9d0af33562d45ba2107753ad3448a253a72fd5fc
Ulrich Petri <ulo@ulo.pe>
2011-07-31 16:25:16 +0200

    Debug serve urls

--- guardhouse/urls.py
+++ guardhouse/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
@@ -31,3 +32,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATICFILES_DIRS[0]}),
+    )
