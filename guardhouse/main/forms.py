20847a1d8257ae7dc8143472d681f26f75034d65
Ulrich Petri <ulo@ulo.pe>
2011-07-31 16:24:28 +0200

    Account config

--- guardhouse/main/forms.py
+++ guardhouse/main/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
-from .models import Site
+from .models import Account, Site
 
 class SiteForm(ModelForm):
     class Meta(object):
         model = Site
         exclude = ('verified',)
 
+class AccountForm(ModelForm):
+    class Meta(object):
+        model = Account
+        exclude = ('owner', 'delegates')
--- guardhouse/main/views.py
+++ guardhouse/main/views.py
@@ -1,5 +1,10 @@
+from django.utils.translation import ugettext as _
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.generic.simple import direct_to_template
 from .decorators import skip_has_account_middleware
+from .forms import AccountForm
+from .models import Account
 
 
 def dashboard(request):
@@ -11,5 +16,29 @@ def settings(request):
 
 
 @skip_has_account_middleware
-def account_setup(request):
-    return direct_to_template(request, 'main/dashboard.html')
+def account_setup(request, force=False):
+    account = None
+    try:
+        account = request.user.account
+    except Account.DoesNotExist:
+        pass
+    form = AccountForm(request.POST if request.method == "POST" else None, instance=account)
+    if request.method == "POST":
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.owner = request.user
+            account.save()
+            messages.add_message(request, messages.INFO, _("Your account has been updated!"))
+            return redirect("account_setup")
+    else:
+        if force:
+            messages.add_message(
+                request, messages.WARNING,
+                _("You need to configure your account on this page before you can use the site.")
+            )
+    return render(request, 'main/account.html', {"form": form})
+
+
+def sites(request):
+    form = None
+    return render(request, 'main/sites.html', {"form": form})
