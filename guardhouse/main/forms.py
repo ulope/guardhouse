c79f30a58cdd8ca01e229d1f99f5b502d154c5ea
Ulrich Petri <ulo@ulo.pe>
2011-07-31 22:04:15 +0200

    Site form update

--- guardhouse/main/forms.py
+++ guardhouse/main/forms.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from .models import Account, Site
 
@@ -5,6 +7,13 @@ class SiteForm(ModelForm):
     class Meta(object):
         model = Site
         exclude = ('belongs_to', 'verification_state',)
+
+    def clean_sentry_key(self):
+        value = self.cleaned_data['sentry_key']
+        if len(value) < 8:
+            raise ValidationError(
+                _("The value is too short. Enter at least 8 characters."))
+        return value
 
 class AccountForm(ModelForm):
     class Meta(object):
