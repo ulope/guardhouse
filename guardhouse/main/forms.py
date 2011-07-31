39e0986f19532433c20a67a7da01ca279f30f000
Ulrich Petri <ulo@ulo.pe>
2011-07-31 16:22:11 +0200

    Fixed form

--- guardhouse/main/forms.py
+++ guardhouse/main/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from .models import Site
 
 class SiteForm(ModelForm):
-    model = Site
-    exclude = ('verified',)
+    class Meta(object):
+        model = Site
+        exclude = ('verified',)
 
