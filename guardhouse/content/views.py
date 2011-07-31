84628158a726864d16d750459a502b94da72eb74
Ulrich Petri <ulo@ulo.pe>
2011-07-31 22:50:24 +0200

    Add a bit explanation to home page

--- guardhouse/content/views.py
+++ guardhouse/content/views.py
@@ -1,7 +1,10 @@
+from django.contrib.sites.models import Site
+from django.shortcuts import render
 from django.views.generic.simple import direct_to_template
 
 def home(request):
-    return direct_to_template(request, "content/home.html")
+    site = Site.objects.get_current()
+    return render(request, "content/home.html", {'site': site})
 
 def impressum(request):
     return direct_to_template(request, "content/impressum.html")
